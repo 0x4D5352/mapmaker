e6dc987ed901498421b8283ec771d66cbb013bdc
change me doofus <changeme@noreply.com>
2024-06-09 11:17:09 -0500

    working wander algorithm

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ tile_weightings = {
     "snow": 0.2,
 }
 x_mark = "❎"
+active_cell = "🟪"
 fps = 120
 framerate = 1 / fps
 
@@ -28,7 +29,6 @@ def grab_random_tile(tiles: dict, weights: dict = tile_weightings) -> str:
 
 def find_neighbors(current_map: list, current_row: int, current_column: int) -> list:
     neighbors = []
-    # print(f"currently at: {current_row},{current_column}")
     if current_column != 0:
         neighbors.append(current_map[current_row][current_column - 1])
     if current_column != len(current_map[0]) - 1:
@@ -46,12 +46,6 @@ def select_neighbor(neighbors: list, tiles: dict) -> str:
     return choice(options)
 
 
-def print_running_map(grid: list) -> None:
-    print(print_map(grid))
-    sleep(framerate)
-    system("clear")
-
-
 def generate_random_neighbor(tiles: dict, grid: list) -> list:
     for r_index, row in enumerate(grid):
         for c_index, _ in enumerate(row):
@@ -64,14 +58,58 @@ def generate_random_neighbor(tiles: dict, grid: list) -> list:
     return grid
 
 
+def grid_wrapper(x: int, table: list) -> int:
+    return x % len(table)
+
+
+def find_nearest_x(row: int, col: int, grid: list) -> tuple:
+    x_row = row
+    x_col = col
+
+    while grid[x_row][x_col] != x_mark:
+        x_row = grid_wrapper((x_row + choice([1, 0, -1])), grid)
+        x_col = grid_wrapper((x_col + choice([1, 0, -1])), grid)
+    return x_row, x_col
+
+
 def generate_wandering_neighbor(tiles: dict, grid: list) -> list:
-    starting_row = choice(range(len(grid)))
-    starting_column = choice(range(len(grid[0])))
-    grid[starting_row][starting_column] = grab_random_tile(tiles)
-    visited = [(starting_row, starting_column)]
-    while len(visited) != len(grid) * len(grid[0]):
-        pass
-    return grid
+    new_grid = grid
+    cardinality = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    current_row = choice(range(len(new_grid)))
+    current_column = choice(range(len(new_grid[0])))
+    new_grid[current_row][current_column] = grab_random_tile(tiles)
+    visited = [(current_row, current_column)]
+    while any(x_mark in column for column in new_grid):
+        current_cell = new_grid[current_row][current_column]
+        new_grid[current_row][current_column] = active_cell
+        print_running_map(new_grid, visited)
+        # choose a direction
+        options = [
+            (
+                grid_wrapper((current_row + x), new_grid),
+                grid_wrapper((current_column + y), new_grid),
+            )
+            for x, y in cardinality
+            if (
+                grid_wrapper(current_row + x, new_grid),
+                grid_wrapper(current_column + y, new_grid),
+            )
+            not in visited
+        ]
+        next_row, next_column = (
+            choice(options)
+            if len(options) > 0
+            else find_nearest_x(current_row, current_column, new_grid)
+        )
+        if new_grid[next_row][next_column] == x_mark:
+            new_grid[current_row][current_column] = current_cell
+            new_grid[next_row][next_column] = select_neighbor(
+                find_neighbors(new_grid, next_row, next_column), tiles
+            )
+            current_row, current_column = next_row, next_column
+            visited.append((current_row, current_column))
+        print_running_map(new_grid, visited)
+    return new_grid
 
 
 def create_map(
@@ -97,6 +135,20 @@ def print_map(map: list) -> str:
     return res
 
 
+def print_running_map(grid: list, visited: list = []) -> None:
+    area = len(grid) * len(grid[0])
+    system("clear")
+    print("completing map...")
+    num_visited = len(visited)
+    if num_visited > 0:
+        # print(sorted(visited))
+        print(f"currently at: {visited[-1][0]},{visited[-1][1]}")
+        print(f"{num_visited} out of {area} seen")
+        print(f"{(num_visited / area) * 100:.2f}% complete!")
+    print(print_map(grid))
+    sleep(framerate)
+
+
 def get_map_dimensions() -> tuple:
     def get_dimension(dimension: str) -> int:
         try:
@@ -112,7 +164,9 @@ def get_map_dimensions() -> tuple:
 
 def main() -> None:
     rows, columns = get_map_dimensions()
-    print(f"{print_map(create_map(map_tiles, rows, columns))}")
+    print(
+        f"{print_map(create_map(map_tiles, rows, columns, generate_wandering_neighbor))}"
+    )
     print("\n")
     response = input("go again? Y for yes, anything else is no\n> ")
     if len(response) > 0:
